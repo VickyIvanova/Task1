a936b03944f0ff74b0d4161b0a70c29966e0c6f1
Админ <@192.168.0.2>
2015-04-11 16:34:21 +0300

    gh

---
+++ ua.kh.kml.ivanova2/src/ua/kh/kml/ivanova2/Parol.java
@@ -0,0 +1,16 @@
+package ua.kh.kml.ivanova2;
+import java.util.Scanner;
+public class Parol {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		String p = scan.next();
+		scan.close();
+		if(p.equals("g5ft6")){
+		System.out.println("Верно");
+		}
+		else System.out.println("Неверно");
+	}
+
+}
